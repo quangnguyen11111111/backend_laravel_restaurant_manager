d5eb1c8de4c443643bf05e5ae395835894ea4855
quangnguyen11111111 <quangnguyen1507204@gmail.com>
2026-04-25 11:46:25 +0700

    Add Requests about updating dishes

--- app/Http/Requests/UpdateDishRequest.php
+++ app/Http/Requests/UpdateDishRequest.php
@@ -2,4 +2,16 @@
 
 namespace App\Http\Requests;
 
-class UpdateDishRequest extends CreateDishRequest {}
+class UpdateDishRequest extends CreateDishRequest
+{
+    // muốn phần cập nhật thì không bắt buộc có ảnh thì phải có s3key
+    public function rules(): array
+    {
+        $rules = parent::rules();
+
+        // Khi cập nhật, ảnh không bắt buộc, nhưng nếu có ảnh thì phải có imageS3Key
+        $rules['imageS3Key'] = ['nullable', 'string', 'max:512'];
+
+        return $rules;
+    }
+}
